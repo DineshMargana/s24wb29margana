2c713b0c4483d534f81967176807821d0fe7b56c
DineshMargana <dineshmargana@gmail.com>
2024-03-26 14:35:56 -0500

    Initial commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var instrumentsRouter = require('./routes/instruments');
+var gridRouter = require('./routes/grid');
+
 
 
 var app = express();
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/instruments', instrumentsRouter);
+app.use('/grid',gridRouter);
+
 
 
 

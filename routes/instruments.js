6150ab14b9dcd77fe3f6605964975fbea6ec51b2
DineshMargana <dineshmargana@gmail.com>
2024-03-26 04:30:17 -0500

    Initial commit

--- routes/instruments.js
+++ routes/instruments.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('instruments', { title: 'Search results of Instruments'});
+  res.render('instruments', { title: 'Search results of instruments'});
 });
 
 module.exports = router;
